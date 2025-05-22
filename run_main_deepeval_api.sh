54c5b09cceec075ab2d48cf8055080ebc67aa04d
maxy-635 <xiangyue_ma@163.com>
2025-05-22 22:57:19 +0800

    0522update

--- run_main_deepeval_api.sh
+++ run_main_deepeval_api.sh
@@ -4,21 +4,13 @@ export PYTHONPATH="/Users/maxy/Documents/pythonProject/my_experiemnt/DeepCodeRAG
 source $(conda info --base)/etc/profile.d/conda.sh
 conda activate DeepCodeRAG
 
-# 加载CUDA和cuDNN模块
-module load cuda/12.1
-module load cudnn/8.9.6.50_cuda12
-
-
 # 定义Python文件的路径
-PYTHON_FILE="./rag/main_deepeval_v3.py"
+PYTHON_FILE="./rag/main_deepeval_api_v3.py"
 
 python3 "$PYTHON_FILE" \
-     --model_local_path "/data/home/sczc725/run/huggingface/hub" \
-     --inference_model_id "google/gemma-2-9b-it" \
-     --embedding_model_id "BAAI/bge-m3" \
+     --inference_model_id "gpt-4o-mini" \
      --api_docs_path "./api_parser/tensorflow/apis_parsed_results" \
-     --embedding_vector_path "./database/api_name_description_detail.faiss" \
      --matching_index_path "./database/whoosh_tf_apis_index_0522" \
      --benchmark "./benchmark/DeepEval/" \
      --repeats 1 \
-     --experiment_id ""
+     --experiment_id "" 
